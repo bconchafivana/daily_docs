843e9d17b2132f2b95d4f214afd3116cd1da2702
bconchafivana <124590930+bconchafivana@users.noreply.github.com>
2023-12-12 11:46:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,14 @@ mem = pd.read_sql(query_mem + docs, conn)
 output = pd.merge(mem, daily, left_on = 'document_id', right_on = 'document_id')
 
 output.to_sql('monthly_closure', engine_ds, index=False, if_exists='replace')
+table_name = 'datascience.monthly_closure'
+
+# Replace 'read_user_bi' and 'read_user_risk' with the actual usernames
+users_to_grant = ['read_user_bi', 'read_user_risk']
+
+# Generate and execute the GRANT commands for each user
+for user in users_to_grant:
+    grant_command = f"GRANT SELECT ON TABLE {table_name} TO {user}"
+    with engine_ds.connect() as connection:
+        connection.execute(grant_command)
 print("monthly_closure uploaded")
