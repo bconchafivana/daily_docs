991ef663ddb0b3d7af4f5cec95ec5fcea0d93725
bconchafivana <124590930+bconchafivana@users.noreply.github.com>
2024-01-04 12:35:05 -0300

    Update main.py
    new finance_amount from json d.operation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from queryes import daily_query, query_mem
 daily = pd.read_sql(daily_query, engine_public)
 
 #financing factoring maybe is not good, so we keep just the one in mem
-daily = daily.drop(columns = ['financing_factoring'])
+daily = daily.drop(columns = ['financing_factoring', 'monto_financiado'])
 #documents per each day
 docs = str(tuple(daily.document_id.tolist()))
 
@@ -15,11 +15,19 @@ mem = pd.read_sql(query_mem + docs, conn)
 output = pd.merge(mem, daily, left_on = 'document_id', right_on = 'document_id')
 
 output.to_sql('monthly_closure', engine_ds, index=False, if_exists='replace')
+
 table_name = 'datascience.monthly_closure'
 
 # Replace 'read_user_bi' and 'read_user_risk' with the actual usernames
-users_to_grant = ['read_user_bi', 'read_user_risk']
+users_to_grant = ['read_user_bi', 'read_user_risk', 'read_user_collections', 'read_user_control' ]
 
+# Generate and execute the GRANT commands for each user
+for user in users_to_grant:
+    grant_command = f"GRANT SELECT ON TABLE {table_name} TO {user}"
+    with engine_ds.connect() as connection:
+        connection.execute(grant_command)
+        
+print("monthly_closure uploaded")
 # Generate and execute the GRANT commands for each user
 for user in users_to_grant:
     grant_command = f"GRANT SELECT ON TABLE {table_name} TO {user}"
